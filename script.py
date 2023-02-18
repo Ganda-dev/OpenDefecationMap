b34b02796893ef5e07ced8bd0185254a370e5277
Gioele Gandaglia <gioganda@hotmail.com>
2023-02-18 15:31:25 +0100

    update

--- script.py
+++
@@ -1,5 +0,0 @@
-import csv
-with open('data.csv', newline='') as csvfile:
-    reader = csv.DictReader(csvfile, delimiter=',')
-    for row in reader:
-        print(row['Location'] + ' - ' + row['Value'] )
